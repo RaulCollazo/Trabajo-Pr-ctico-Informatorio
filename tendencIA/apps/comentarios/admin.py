f0828451b44b3357073cbef9cd0f258819b41487
RaúlCollazo <raulcollazochachich23@gmail.com>
2025-08-15 21:25:59 -0300

    ajam

--- tendencIA/apps/comentarios/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Comentario
-
-@admin.register(Comentario)
-class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'post', 'usuario', 'fecha_hora')
-    search_fields = ('contenido', 'post__titulo', 'usuario__username')
-    list_filter = ('fecha_hora', 'usuario')
-    date_hierarchy = 'fecha_hora'
